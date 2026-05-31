46b7a0bf87b1c3894373149776e9e2a6b9f34646
Alexandr Rusakov <alexanderrusakov01@mail.ru>
2026-05-31 17:09:29 +0400

    update for Jenkins

--- conftest.py
+++ conftest.py
@@ -1,7 +1,5 @@
 import pytest
 from selene import browser
-from selenium import webdriver
-
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_setup():
@@ -10,9 +8,6 @@ def browser_setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.timeout = 6
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = "eager"
-    browser.config.driver_options = driver_options
 
     yield
     browser.quit()
